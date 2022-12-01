7e33aefb5e0d8bb620de69751715cd4e0c77e8d3
VictorChacon <vchacon@jaumebalmes.net>
2022-12-01 19:24:35 +0100

    0

---
+++ app/src/main/java/com/example/susin1/FusionLayout.java
@@ -0,0 +1,14 @@
+package com.example.susin1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FusionLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fusion_layout);
+    }
+}
